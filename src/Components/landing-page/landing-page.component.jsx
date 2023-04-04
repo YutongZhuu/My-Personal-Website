50c5a89b1b9b6fa85f9918fadb84c4d5234edce6
Yutong Zhu <lbwfeichangnb@gmail.com>
2023-04-03 23:09:49 -0400

    built animation of fadeIn, and implemented useEffect hook to make sure it is working

--- src/Components/landing-page/landing-page.component.jsx
+++ src/Components/landing-page/landing-page.component.jsx
@@ -1,6 +1,25 @@
 import './landing-page.styles.scss'
 import Button from '../Button/button.component';
+import { fadeIn } from '../../Animation/fade-in.animate';
+import { useEffect, useState } from 'react';
 const LandingPage = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const pageTitle = document.querySelector('.landing-page-title')
+        const px = 20 * window.innerWidth / 100
+        fadeIn(pageTitle, 'left', px, 500)
+    }, [windowWidth])
+
+    useEffect(() => {
+        function handleResize() {
+          setWindowWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+      
     return (
         <>
             <div className="landing-page">
