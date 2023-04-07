e92676d778601eb7bc244009daf80415080d6a4c
Yutong Zhu <lbwfeichangnb@gmail.com>
2023-04-06 21:01:04 -0400

    fixed an issue with naming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import LandingPage from './Components/Landing-page/landing-page.component';
+import LandingPage from './Components/landing-page/landing-page.component';
 import Navigation from './Components/Navigation/navigation.component';
 import AboutMe from './Components/About-me/about-me.component';
 // import './App.css';
