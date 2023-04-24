6b0a955a66ba3f27ad0ec2ebce1de840d99de76b
Yutong Zhu <Yutong2022@gmail.com>
2023-04-24 15:57:18 -0400

    updated the function for scrolling animation

--- src/Animation/scroll.animate.js
+++ src/Animation/scroll.animate.js
@@ -1,16 +1,41 @@
-export const scroll = (targetValue, duration) => {
-    const startValue = parseInt(window.scrollY);
-    const distance = targetValue+10 - startValue;
-    const startTime = performance.now();
-    function animationStep() {
-        const elapsed = performance.now() - startTime;
-        const progress = Math.min(elapsed / duration, 1);
-        const currentValue = startValue + distance * progress;
-        window.scrollTo(0, currentValue);
-        if (progress < 1) {
-            requestAnimationFrame(animationStep);
-        }
-    }
+// export const scroll = (targetValue, duration) => {
+//     const startValue = parseInt(window.scrollY);
+//     const distance = targetValue+10 - startValue;
+//     const startTime = performance.now();
+//     function animationStep() {
+//         const elapsed = performance.now() - startTime;
+//         const progress = Math.min(elapsed / duration, 1);
+//         const currentValue = startValue + distance * progress;
+//         window.scrollTo(0, currentValue);
+//         if (progress < 1) {
+//             requestAnimationFrame(animationStep);
+//         }else{
+//             console.log('stop')
+//         }   
+//      }
 
-    requestAnimationFrame(animationStep);
+//     requestAnimationFrame(animationStep);
+// }
+
+
+let requestId;
+
+function animation(target) {
+  const currentValue = parseInt(window.scrollY);
+  let step = (target - currentValue) / 20;
+  if (step < 0 && step > -1) {
+    step = -1;
+  } else if (step < 1 && step > 0) {
+    step = 1;
+  }
+  window.scrollBy(0, step);
+  if (Math.abs(target - currentValue)!==0) {
+    requestId= requestAnimationFrame(() => animation(target));
+  }else{
+    // return false;
+  }
 }
+export const scroll = (target) => {
+  cancelAnimationFrame(requestId);
+  animation(target);
+};
--- src/Components/Navigation/navigation.component.jsx
+++ src/Components/Navigation/navigation.component.jsx
@@ -1,36 +1,76 @@
+/*
+A React component for a navigation bar.
+The component uses useState and useEffect hooks from React for state management and side-effect handling respectively.
+The component renders either an IconList or a WordList component based on the orientation prop.
+*/
 import './navigation.styles.scss';
-
 import { useEffect, useState } from 'react';
 import { scroll } from '../../Animation/scroll.animate';
 import LinkLists from '../Link-list/link-list';
 import IconList from '../Icon-List/Icon-List';
 import WordList from '../Word-List/word-list.component';
-const Navigation = ({orientation}) => {
-    const findBetweenArrayElements = (number, array) => {
-        for (let i = 0; i < array.length; i++) {
-            if (number > array[i] && (array[i + 1] ? number < array[i + 1] : true)) {
-                return i;
-            }
+
+const findBetweenArrayElements = (number, array) => { //Function that is used for determine which section the viewer is in
+    for (let i = 0; i < array.length; i++) {
+        if (number >= array[i] && (array[i + 1] ? number < array[i + 1] : true)) {
+            return i;
+        } else if (number == 0) {
+            return 0;
         }
     }
+}
+
+const Navigation = ({ orientation }) => {
 
     const [headers, setHeaders] = useState(null);
     const [iconClicked, setIconClicked] = useState(false)
+
+
+    const onHoverHandler = (event) => {//Function triggers when mouseover
+        setIconClicked(true)
+        if (event.target.tagName == 'LI' || event.target.tagName == 'P') {
+            while (event.target.tagName != 'LI') {
+                event.target = event.target.parentNode;
+            }
+            for (let i = 0; i < event.target.parentNode.childNodes.length; i++) {
+                event.target.parentNode.childNodes[i].className = ""
+            }
+            event.target.className = 'target-icon';
+        }
+    }
+
+    const onMouseLeaveHandler = () => {//Function used for mouseLeave
+        setIconClicked(false)
+        const scrollEvent = new Event('scroll');
+        document.dispatchEvent(scrollEvent);
+    }
+
+    const onClickHandler = (event) => {//Function used for the click Event
+        let target = event.target;
+        if (target.tagName != 'LI' && target.tagName != 'svg' && target.tagName != 'path' && target.tagName != 'P')
+            return
+        while (target && target.tagName !== 'LI') {
+            target = target.parentNode;
+        }
+
+        const iconNumber = Array.from(target.parentNode.childNodes).findIndex((i) => i == target);
+        scroll(headers[iconNumber]);
+    }
+    
     useEffect(() => {
-        onMouseLeaveHandler();
         const headersArray = Array.from(document.querySelectorAll('h1')).map((h, i) => {
             if (i == 0)
                 return 0;
             else
-                return h.offsetTop-90;
+                return h.offsetTop - 90;
         });
         setHeaders(headersArray)
-        const nav_bar=document.getElementsByClassName('nav-bar')
+        const nav_bar = document.getElementsByClassName('nav-bar')
         const scrollHandler = () => {
-            if(window.scrollY>50){
-                nav_bar[0].className='nav-bar nav-background'
-            }else{
-                nav_bar[0].className='nav-bar'
+            if (window.scrollY > 50) {
+                nav_bar[0].className = 'nav-bar nav-background'
+            } else {
+                nav_bar[0].className = 'nav-bar'
             }
             if (iconClicked) return
             const icons = document.querySelectorAll('.navigation li')
@@ -41,57 +81,25 @@ const Navigation = ({orientation}) => {
             iconNumber ? icons[iconNumber].className = 'target-icon' : icons[0].className = 'target-icon'
         }
         document.addEventListener('scroll', scrollHandler)
-
+        const scrollEvent = new Event('scroll');
+        document.dispatchEvent(scrollEvent);
         return () => {
             document.removeEventListener('scroll', scrollHandler);
         };
     }, [iconClicked])
-
-    const onHoverHandler = (event) => {
-        if (event.target.tagName == 'LI'|| event.target.tagName == 'P') {
-            while(event.target.tagName!='LI'){
-                event.target=event.target.parentNode;
-            }
-            for (let i = 0; i < event.target.parentNode.childNodes.length; i++) {
-                event.target.parentNode.childNodes[i].className = ""
-            }
-            event.target.className = 'target-icon';
-        }
-    }
-
-    const onMouseLeaveHandler = () => {
-        const scrollEvent = new Event('scroll');
-        document.dispatchEvent(scrollEvent);
-    }
-
-    const onClickHandler = (event) => {
-        setIconClicked(true);
-        let target = event.target;
-        if (target.tagName != 'LI' && target.tagName != 'svg' && target.tagName != 'path' &&  target.tagName != 'P')
-            return
-        while (target && target.tagName !== 'LI') {
-            target = target.parentNode;
-        }
-
-        const iconNumber = Array.from(target.parentNode.childNodes).findIndex((i) => i == target);
-        scroll(headers[iconNumber], 1200);
-        setTimeout(() => {
-            setIconClicked(false);
-        }, 500)
-    }
 
     return (
 
         <div className='nav-bar'>
             <LinkLists />
             <div className="navigation">
-                {orientation?<IconList
-                onMouseLeaveHandler={onMouseLeaveHandler}
-                onHoverHandler={onHoverHandler}
-                onClickHandler={onClickHandler}/>:<WordList
-                onMouseLeaveHandler={onMouseLeaveHandler}
-                onHoverHandler={onHoverHandler}
-                onClickHandler={onClickHandler}/>}
+                {orientation ? <IconList
+                    onMouseLeaveHandler={onMouseLeaveHandler}
+                    onHoverHandler={onHoverHandler}
+                    onClickHandler={onClickHandler} /> : <WordList
+                    onMouseLeaveHandler={onMouseLeaveHandler}
+                    onHoverHandler={onHoverHandler}
+                    onClickHandler={onClickHandler} />}
             </div>
         </div>
     );
